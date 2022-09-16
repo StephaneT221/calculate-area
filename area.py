d5ad05a49d5fa2589682b44d5efcbd5b0d2574c5
StephaneT221 <113712351+StephaneT221@users.noreply.github.com>
2022-09-16 14:21:42 -0400

    Add files via upload

---
+++ area.py
@@ -0,0 +1,12 @@
+def square(s):
+  return  s ** 2
+
+def rectangle(l, w):
+  return l * w
+
+def circle(r):
+  return 3.14 * r ** 2
+
+def triangle (b, h):
+  return 1 / 2 * b * h
+
---
+++ main (2).py
@@ -0,0 +1,6 @@
+import area
+print(area.square(5))
+
+print(area.triangle(4, 2))
+
+print(area.circle(3))
